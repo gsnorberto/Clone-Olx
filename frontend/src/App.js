3dfeb1d5d8e381b1d86e5175ee48f918945fe8be
Gabriel Silva Norberto <sn_gabriel@outlook.com>
2022-01-22 16:59:44 -0300

    Foi configurado todos os arquivos iniciais do projeto para começo do desenvolvimento do Sistema

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Page = (props) => {
+   return (
+      <div>Opa, funcionando...</div>
+   )
+}
+
+const mapStateToProps = (state) => {
+   return {
+      user: state.user
+   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+   return{
+
+   };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
